bee97b80d2c6e4cc2e2e314eac26e89688d8abfc
irul45 <hudairul314@gmail.com>
2023-11-09 10:32:06 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let nama ="fita azzahra"
+let kelas ="sija 11"
+let umur ="17 tahun"
+let jago ="MATEMATIKA"
+let gakpercaya ="saya buktikan"
+
+let saldoAwal = "5000"
+let saldoTambahan = "8000"
+
+const saldoAkhir = saldoAwal + saldoTambahan 
+
+const x = 10
+const y = 50
+const z = x * y
+alert(`perkenalkan nama saya adalah ${nama}`)
+alert(`sekarang saya kelas ${kelas}`)
+alert(`umur saya sekarang ditahun 2023 adalah ${umur}`)
+alert(`saya jago dimata pelajaran ${jago}`)
+alert(`kalau gak percaya ${gakpercaya}`)
+alert(`nilai x = ${x} * nilai y = ${y} maka hasilnya adalah ${z}`,)
